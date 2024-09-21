84c2578c46e14e4b14e49c0019e0c9c9e8b5cc35
Ashler18 <delekeashler@gmail.com>
2024-09-21 21:59:06 +0200

    Update trigonometrie.py

--- mathfacile/trigonometrie.py
+++ mathfacile/trigonometrie.py
@@ -9,3 +9,8 @@ def calculer_cosinus(angle):
 def calculer_tangente(angle):
     """Calcule la tangente de l’angle donné en radians."""
     return math.tan(angle)
+
+
+def calculer_sinus(angle):
+    """Calcule le sinus de l’angle donné en radians."""
+    return math.sin(angle)
